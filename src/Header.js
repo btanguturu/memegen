f868f4b0595ff374aff0884f2bf8397781e9558f
Bhavya <btanguturu@eagles.ewu.edu>
2019-12-06 13:16:42 -0800

    updated img link

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ function Header() {
     return (
         <header>
             <img 
-                src="../img/MemeCat" 
+                src="../img/MemeCat.png" 
                 alt="Cat?"
             />
             <p>Meme Generator</p>
